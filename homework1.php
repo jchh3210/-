47b80f2e568e19f003f7722e1bbee155f0a79e45
jchh3210 <127706255+jchh3210@users.noreply.github.com>
2023-04-17 10:20:50 +0900

    Add files via upload

---
+++ homework1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$n = 30;
+$sum = 0;
+$prod = 1;
+for($i=1; $i<=$n; $i++){
+  echo $i." ";
+  $sum += $i;
+  $prod *= $i;
+}
+echo "<br>";
+echo "1 + ... + 30 = ".$sum."<br>";
+echo "1 * ... * 30 = ".$prod;
+?>
+
+</body>
+</html>
